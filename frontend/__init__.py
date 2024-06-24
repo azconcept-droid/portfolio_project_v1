83a59e1bc3c397be2fc0bef6580d6ef96dcc685c
Yahaya Azeez <azconcept2016@gmail.com>
2024-06-24 15:49:15 +0100

    add init file for python package

---
+++ frontend/__init__.py
@@ -0,0 +1,16 @@
+#!myvenv/bin/python3
+"""Initializes the Flask app"""
+
+from flask import Flask
+# from config import Config
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+# app.config.from_object(Config)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
+from frontend import index
+from models import agent, customer
