768359effb5085bd3db8f50228158340c277f313
Yahaya Azeez <azconcept2016@gmail.com>
2024-06-24 15:53:13 +0100

    add model packages

---
+++ models/customer.py
@@ -0,0 +1,18 @@
+# #!venv/bin/python3
+# """ Customer model module """
+# from typing import Optional
+# import sqlalchemy as sa
+# import sqlalchemy.orm as so
+# from app import db
+
+# class Customer(db.Model):
+#     id: so.Mapped[int] = so.mapped_column(primary_key=True)
+#     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+#                                                 unique=True)
+#     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+#                                              unique=True)
+#     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+#     def __repr__(self):
+#         return '<User {}>'.format(self.username)
+    
