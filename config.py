ecd00dfef6197a59fbd36dd803b15121dd17ff7d
Yahaya Azeez <azconcept2016@gmail.com>
2024-06-25 17:03:02 +0100

    add config file

---
+++ config.py
@@ -0,0 +1,6 @@
+""" Configuration module """
+import os
+
+class Config:
+    """ Configuration class """
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
--- frontend/__init__.py
+++ frontend/__init__.py
@@ -2,13 +2,13 @@
 """Initializes the Flask app"""
 
 from flask import Flask
-# from config import Config
+from config import Config
 # from flask_sqlalchemy import SQLAlchemy
 # from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-# app.config.from_object(Config)
+app.config.from_object(Config)
 # db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
 
